1df709ecf9998d6e9c2274c1a49928d2b33401d8
qiufg <qfguang@163.com>
2017-03-14 16:29:34 +0800

    Toast工具类，多次show不重叠

---
+++ app/src/main/java/com/qiufg/util/Toast.java
@@ -0,0 +1,23 @@
+package com.qiufg.util;
+
+import android.content.Context;
+
+/**
+ * Description 多次Toast不重叠
+ * Author qiufg
+ * Date 2017/3/14 16:23
+ */
+
+public class Toast {
+
+    private static android.widget.Toast sToast;
+
+    public static void show(Context mContext, String message) {
+        if (sToast == null) {
+            sToast = android.widget.Toast.makeText(mContext, message, android.widget.Toast.LENGTH_LONG);
+        } else {
+            sToast.setText(message);
+        }
+        sToast.show();
+    }
+}
