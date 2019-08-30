0b9ab365af1525e5ff7a0ce93b0eb2345324c3bc
fengguang.qiu <qfguang@163.com>
2019-08-30 11:51:47 +0800

    main导航

--- mvp/src/main/java/com/qiufg/mvp/module/main/view/MainActivity.java
+++ mvp/src/main/java/com/qiufg/mvp/module/main/view/MainActivity.java
@@ -1,14 +1,35 @@
 package com.qiufg.mvp.module.main.view;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
-import android.os.Bundle;
+import androidx.viewpager.widget.ViewPager;
 
 import com.qiufg.mvp.R;
 import com.qiufg.mvp.module.base.BaseActivity;
 import com.qiufg.mvp.module.main.presenter.MainPresenter;
+import com.qiufg.mvp.wedget.CustomViewPager;
 
-public class MainActivity extends BaseActivity<MainPresenter> implements MainView {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity<MainPresenter> implements MainView, ViewPager.OnPageChangeListener {
+
+    private static final int NAVI_INDEX_HOME = 0;
+    private static final int NAVI_INDEX_CATEGORY = 1;
+    private static final int NAVI_INDEX_SERVICE = 2;
+    private static final int NAVI_INDEX_MINE = 3;
+    @BindView(R.id.viewPager)
+    CustomViewPager mViewPager;
+    @BindView(R.id.ll_home)
+    LinearLayout mLayoutHome;
+    @BindView(R.id.ll_category)
+    LinearLayout mLayoutCategory;
+    @BindView(R.id.ll_service)
+    LinearLayout mLayoutService;
+    @BindView(R.id.ll_mine)
+    LinearLayout mLayoutMine;
 
     @Override
     protected int createView() {
@@ -22,6 +43,63 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainVie
 
     @Override
     protected void viewCreated() {
+        ButterKnife.bind(this);
+        mViewPager.addOnPageChangeListener(this);
+    }
 
+    @OnClick({R.id.ll_home, R.id.ll_category, R.id.ll_service, R.id.ll_mine})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ll_home:
+                mViewPager.setCurrentItem(NAVI_INDEX_HOME);
+                tabSelected(mLayoutHome);
+                break;
+            case R.id.ll_category:
+                mViewPager.setCurrentItem(NAVI_INDEX_CATEGORY);
+                tabSelected(mLayoutCategory);
+                break;
+            case R.id.ll_service:
+                mViewPager.setCurrentItem(NAVI_INDEX_SERVICE);
+                tabSelected(mLayoutService);
+                break;
+            case R.id.ll_mine:
+                mViewPager.setCurrentItem(NAVI_INDEX_MINE);
+                tabSelected(mLayoutMine);
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch (position) {
+            case NAVI_INDEX_HOME:
+                tabSelected(mLayoutHome);
+                break;
+            case NAVI_INDEX_CATEGORY:
+                tabSelected(mLayoutCategory);
+                break;
+            case NAVI_INDEX_SERVICE:
+                tabSelected(mLayoutService);
+                break;
+            case NAVI_INDEX_MINE:
+                tabSelected(mLayoutMine);
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+
+    private void tabSelected(LinearLayout layout) {
+        mLayoutHome.setSelected(false);
+        mLayoutCategory.setSelected(false);
+        mLayoutService.setSelected(false);
+        mLayoutMine.setSelected(false);
+        layout.setSelected(true);
     }
 }
